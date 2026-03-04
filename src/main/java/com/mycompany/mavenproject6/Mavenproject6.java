7038f59a90a6b542520c57b30e7c42571fd73c30
erpkbishnoi@gmail.com <erpkbishnoi@gmail.com>
2026-03-04 15:02:19 +0530

    dhdfhdfh

---
+++ src/main/java/com/mycompany/mavenproject6/Mavenproject6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject6;
+
+/**
+ *
+ * @author Dell
+ */
+public class Mavenproject6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("ssssssssssssssss");
+    }
+}
